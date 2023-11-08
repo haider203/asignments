41326244c3eacbc23354483cd6213214271db4dc
haider ali tariq <140200162+haider203@users.noreply.github.com>
2023-11-08 05:51:54 -0800

    modern python it will help to alot

---
+++ class1.py
@@ -0,0 +1,19 @@
+# print("hello world")
+
+
+# name : str = "haider ali tariq"
+
+# card : str = "piaic student card\nStudent Name:"
+
+import sys
+
+print("line1")
+print("line2")
+
+print(type(sys.srgv))
+
+print(sys.argv)
+
+
+
+
