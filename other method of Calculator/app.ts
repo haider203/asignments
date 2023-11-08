76ff8ddc3b49cd24ab4f5ef0eb466bd6f6c5bfc7
haider ali tariq <140200162+haider203@users.noreply.github.com>
2023-11-08 05:54:01 -0800

    another calculator

---
+++ other method of Calculator/app.ts
@@ -0,0 +1,70 @@
+// If parent class provide constructor(with or without argument) 
+//then you must have to create constructor in child class
+// and must call super()
+// Below code will not work because it does not call super,
+
+// Case 1:
+// If parent class provide constructor and child class does not
+// provide then child class will use parent's constrcutor
+// and child's object creation will required use of parent's 
+//constructor parameters 
+class A {
+    name:string;
+    constructor(theName: string, age:number) { 
+        this.name = theName; 
+        console.log("A constrcutor");
+    }
+}
+class B extends A {
+
+}
+let a: A = new A("A",4); // This is working as expected
+let b: B = new B("C",8); // This is working too as child class use parents constructor
+
+
+// Case 2:
+// If parent class does not provide constructor and child class provide constrcutor then 
+// child class must call super() within child's class constructor
+class C {
+}
+class D extends C {
+    name:string;
+    constructor(theName: string,age:number) {
+        super();
+        this.name = theName; 
+        console.log("D constrcutor");
+         
+    }
+}
+let aa: C = new C(); // This is working as expected
+
+
+// Case 3:
+// If parent class and child class both provide constructor 
+// then child class must call super with same parameters as 
+// they are in parent's constructor call to super
+class E {
+    name:string;
+    constructor(theName: string,age:number) { 
+        this.name = theName; 
+        console.log("E constrcutor");
+    }
+    displayName():void {
+        console.log("Name = " + this.name);
+    }
+}
+class F extends E {
+    name:string;
+    constructor(theName: string) {
+        super(theName,4);
+        // Must call super with two arguments
+        this.name = theName; 
+        console.log("F constrcutor");
+          
+        
+        //super("Hello",5); // Multiple supper call working, No error here -- seems to be some special case   
+    }
+}
+let e: E = new E("E",1);
+let f: F = new F("F");
+f.displayName(); // Display "F"
